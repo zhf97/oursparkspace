4654b95e6a9f21446fbe4112d7f62424708752e9
zhf97 <zhanghanfei@bupt.edu.cn>
2018-04-20 17:19:03 +0800

    Add files via upload

---
+++ HLSPlayer/index.php
@@ -0,0 +1,50 @@
+﻿<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,minimal-ui" />
+<meta charset="utf-8">
+<title>闫浩的数学教室</title>
+<meta name="keywords" content="闫浩" />
+<meta name="description" content="闫浩的数学教室" />
+</head>
+
+<body>
+
+
+<!--HLSPlayer代码开始-->
+<div class="video" id="HLSPlayer" >
+<SCRIPT LANGUAGE=JavaScript>
+<!--
+/*
+* HLSPlayer参数应用=========================<br>
+* @Contact QQ:261532593 
+* @param {Object} vID        ID
+* @param {Object} vWidth     播放器宽度设置
+* @param {Object} vHeight    播放器宽度设置
+* @param {Object} vPlayer    播放器文件
+* @param {Object} vHLSset    HLS配置
+* @param {Object} vPic       视频缩略图
+* @param {Object} vCssurl    移动端CSS应用文件
+* @param {Object} vHLSurl    HLS(m3u8)地址
+* ==========================================
+*/
+var vID        = ""; 
+var vWidth     = "100%";                //播放器宽度设置
+var vHeight    = 800;                   //播放器宽度设置
+var vPlayer    = "HLSplayer.swf?v=1.5"; //播放器文件
+var vHLSset    = "HLS.swf";             //HLS配置
+var vPic       = "start.jpg";    //视频缩略图
+var vCssurl    = "";     //移动端CSS应用文件
+
+//HLS(m3u8)地址,适配PC,安卓,iOS,WP
+var vHLSurl    = "http://jl-lagrange.com.cn/video/edu/edu.m3u8";
+
+//-->
+</SCRIPT> 
+<script type="text/javascript" src="js/hls.min.js?rand=20141217"></script>
+</div>
+<!--HLSPlayer代码结束-->
+
+
+</body>
+</html>
